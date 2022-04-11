5ff53239e2facbaca11053f0a77e0a73e04a3cac
attalariq123 <attalariq002@gmail.com>
2022-04-11 23:21:23 +0700

    make orders table

---
+++ app/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $table = 'orders';
+
+    protected $fillable = [
+        'user_id',
+        'destination_id',
+        'start_date',
+        'end_date',
+        'ticket_quantity',
+        'total_amount',
+        'payment_status',
+    ];
+}
